964541449ead220d7e8d35c750e7317aa8e7771c
smiroshnikov <sergeim.qa@gmail.com>
2015-12-23 00:50:45 +0200

    histogram spread and totals

--- Dario/unit06/seker.c
+++ Dario/unit06/seker.c
@@ -15,8 +15,8 @@ void print_array(int arr[], int size);
 void printHistogram(int *score, int size);
 
 int main() {
-    int myArr[] = {1, 3, 6, 4, 7, 8, 3, 9, 8, 7, 6, 5, 5, 4, 3, 6, 7, 6, 5, 4, 3, 7, 3,
-                   1, 2, 3, 6, 5, 4, 5, 6, 7, 4, 8, 1, 6, 7, 8, 9, 5, 3, 3, 2, 0, 5, 6, 7, 1};
+    int myArr[] = {1, 1, 1, 1, 1, 1, 3, 3, 8, 8, 8, 8, 5, 4, 3, 6, 8, 8, 8, 8, 8, 7, 3,
+                   1, 2, 3, 6, 5, 4, 5, 6, 7, 4, 8, 1, 6, 8, 8, 9, 5, 3, 3, 2, 8, 5, 6, 7, 1};
     // int small_arr[] = {1, 1, 1, 2, 2, 3, 3, 3, 3};
     int size = sizeof(myArr) / sizeof(myArr[0]);
     //  int size = sizeof(small_arr) / sizeof(small_arr[0]);
@@ -47,9 +47,10 @@ void print_array(int arr[], int size) {
 void printHistogram(int score[], int size) {
     int i, j;
     for (i = 0; i < size; i++) {
-        printf("%d: ", i);
+        printf("Grade : %d ", i);
         for (j = 0; j < score[i]; j++)
             printf("*");
+        printf("\t total count : %d\t", j);
         printf("\n");
     }
 }
