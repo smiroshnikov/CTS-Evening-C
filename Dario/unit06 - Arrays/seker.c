e4d9f1c6bbc3662bfc6dabe0fd431ad6cc4fdc2e
smiroshnikov <sergeim.qa@gmail.com>
2015-12-23 00:31:43 +0200

    fixed array definition

--- Dario/unit06 - Arrays/seker.c
+++ Dario/unit06 - Arrays/seker.c
@@ -12,11 +12,11 @@
 
 void print_array(int arr[], int size);
 
-void printHistogramm(int score[], int size);
+void printHistogram(int *score, int size);
 
 int main() {
     int myArr[] = {1, 3, 6, 4, 7, 8, 3, 9, 8, 7, 6, 5, 5, 4, 3, 6, 7, 6, 5, 4, 3, 7, 3,
-            //       1,2,3,6,5,4,5,6,7,4,8,1,6,7,8,9,5,3,3,2,0,5,6,7,1};
+                   1, 2, 3, 6, 5, 4, 5, 6, 7, 4, 8, 1, 6, 7, 8, 9, 5, 3, 3, 2, 0, 5, 6, 7, 1};
             // int small_arr[] = {1, 1, 1, 2, 2, 3, 3, 3, 3};
             int size = sizeof(myArr)/sizeof(myArr[0]);
             int size = sizeof(small_arr) / sizeof(small_arr[0]);
