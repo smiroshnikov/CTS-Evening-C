bdb1cce9a3b6f70551cfc485248c9281f241f924
smiroshnikov <sergeim.qa@gmail.com>
2015-12-23 23:58:54 +0200

    switched to int

--- Lior-homework/homework-5/arrays1.c
+++ Lior-homework/homework-5/arrays1.c
@@ -10,26 +10,27 @@ void minimum_within_array(int [], int);
 
 void print_array_from_large_small(int [], int);
 
-float average_in_array(float [], int);
+float average_in_array(int [], int);
 
 int main() {
-    float A[N] = {2.0f, 2.0f, 2.0f, 3.0f, 1.5f, 1.5f};
+    //float A[N] = {2.0f, 2.0f, 2.0f, 3.0f, 1.5f, 1.5f};
+    int A[N] = {2, 2, 2, 3, 3, 6};
     int arraySize = sizeof(A) / (sizeof(int));
     for (int i = 0; i < arraySize; i++) {
-        printf("%.1f\t", A[i]);
+        printf("%d\t", A[i]);
     }
     printf("\nLen(A) %d\n", arraySize);
     printf("Average is %.2f", average_in_array(A, arraySize));
     return 0;
 }
 
-float average_in_array(float A[], int arraySize) {
+float average_in_array(int A[], int arraySize) {
     float average;
-    float sum = 0;
+    int sum = 0;
     for (int i = 0; i < arraySize; i++) {
         sum += A[i];
     }
-    printf("Sum is %.1f\n", sum);
+    printf("Sum is %d\n", sum);
     average = sum / arraySize;
     return average;
 }
