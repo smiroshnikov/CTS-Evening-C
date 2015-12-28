867685f362ba26094bcdabf787ea5dede80a1843
smiroshnikov <sergeim.qa@gmail.com>
2015-12-28 22:49:28 +0200

    even odd solution from Dario and string lesson 2

---
+++ Dario/Dario-challenges/dario-classwork-strings.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+int my_strLen(char []);
+
+void my_strCat(char [], char []);
+
+void my_strCpy(char [], char []);
+
+// need to build our own str function frpm below
+
+int main() {
+
+    char s1[20] = "12345";
+    char s2[10] = "abcdef";
+
+    printf("strlen (s1) = %ld\n", strlen(s1));
+    printf("strcmp (s1,s2) = %d\n\n", strcmp(s1, s2));
+    printf("strcat (s1,s2) = %s\n\n", strcat(s1, s2));
+    printf("strcpy (s1,s2) = %s\n\n", strcpy(s1, s2));
+
+
+    //   printf("strcpy (s1,s2) = %s\n\n", strcpy(s1, s2));
+
+    printf("my_len [%d] \n", my_strLen(s1));
+    my_strCat(s1, s2);
+    puts(s1);
+    my_strCpy(s1, s2);
+    puts(s1);
+
+
+    return 0;
+
+}
+
+int my_strLen(char STR[]) {
+    int len = 0;
+    while (STR[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+void my_strCat(char A[], char B[]) {
+    int i = 0;
+    int j = my_strLen(A); // was inside loop , had a race condition!
+
+    for (i = 0; i < my_strLen(A); i++) {
+        A[j + i] = B[i];
+    }
+
+}
+
+void my_strCpy(char A[], char B[]) {//s2->s1 s2>s1
+    int i = 0;
+
+    for (i = 0; i < my_strLen(B); i++) {
+        A[i] = B[i];
+    }
+    A[i] = '\0';
+
+
+}
--- Dario/Dario-challenges/pidaras.c
+++ Dario/Dario-challenges/pidaras.c
@@ -3,58 +3,41 @@
 //
 #include <stdio.h>
 
-void PRINT_ARRAY(int [], int);
 
-void EVEN_ODD(int [], int);
+void my_getLen(char []);
 
 
 int main() {
-    int userArray[] = {5, 4, 4, 6, 8};
-    int size = sizeof(userArray) / sizeof(userArray[0]);
-    PRINT_ARRAY(userArray, size);
-    EVEN_ODD(userArray, size);
-    PRINT_ARRAY(userArray, size);
+
     return 0;
+
 }
 
-
-void PRINT_ARRAY(int userArray[], int sizeOfArray) {
+void PRINT_ARRAY(int A[], int size) {
     int i = 0;
 
-    for (i = 0; i < sizeOfArray; i++)
-        printf("userArray[ %d] = %d \n", i, userArray[i]);
+    for (i = 0; i < size; i++)
+        printf("A[ %d] = %d \n", i, A[i]);
 }
 
+void evenOddArr(int A[], int size) {
 
-
-// works in simple case {ODD,EVEN,EVEN,EVEN,EVEN}
-// have problem with 2 ODDS
-
-void EVEN_ODD(int userArray[], int sizeOfArray) {
-    int i;
+    int i = 0;
+    int j = size - 1;
     int temp;
-    int even_position = 0;
-    int odd_position = 0;
-    // position trackers
-    for (i = 0; i < sizeOfArray; i++) {
-        if (userArray[i] % 2 == 0) {
-            // STORE position
-            even_position = i;
-            //   printf("EVEN %d @ [%d] position \n", userArray[i], i);
+
+    while (i < j) {
+        if (A[i] % 2 == 0) {
+            i++;
+            continue;
         }
-        else {
-            // STORE position
-            odd_position = i;
-            //    printf("ODD %d @ [%d] position \n", userArray[i], i);
+        if (A[j] % 2 != 0) {
+            j--;
+            continue;
         }
+        temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
-    // lets replace KEEP EVEN MOVE ODD
-    // no decision here just flat bullshit
-    for (i = 0; i < sizeOfArray; i++) {
-        if (userArray[i] % 2 != 0) {
-            temp = userArray[odd_position];
-            userArray[odd_position] = userArray[even_position];
-            userArray[even_position] = temp;
-        }
-    }
+
 }
