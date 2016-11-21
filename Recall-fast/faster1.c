2dac7feb64e9cbd463aaff9eca71d4d5da382a9f
smiroshnikov <sergeim.qa@gmail.com>
2016-11-21 23:51:10 +0200

    recalling the basics

--- Recall-fast/faster1.c
+++ Recall-fast/faster1.c
@@ -1,9 +1,26 @@
-//
-// Created by Iidwuurliik on 11/21/2016.
-//
+#include<stdio.h>
 
-#include <stdio.h>
-void main() {
+int main(int argc, char *argv[]) {
+    int distance = 100;
+    int age = 191;
+    int y = -100;
+    long how_big_is_this = 1L * 1024L * 1024L * 1024L;
+    double irrational_number = 4.54654654;
+    //char nul_byte = '\0';
+    char nul_byte = 'z';
+    int result = y * nul_byte;
 
-    printf("Hey");
+    printf("Distance is  %d miles away ", distance);
+    printf("I am %d years old and feel perfectly fine ! ", age);
+    if (age > 0)
+        goto fail;
+    fail :
+    printf("FAILED y is %d", y);
+
+    printf("This is %ld \n", how_big_is_this);
+    printf("This is float %.3f \n", irrational_number);//works!
+    printf("Multiplied char with int %d \n",result);
+
+
+    return 0;
 }
