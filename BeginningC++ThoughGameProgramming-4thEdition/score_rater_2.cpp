f06dd26de6e3b656d4e658d2311475967d722948
smiroshnikov <sergeim.qa@gmail.com>
2016-12-07 21:09:58 +0200

    game loop , try harder

--- BeginningC++ThoughGameProgramming-4thEdition/score_rater_2.cpp
+++ BeginningC++ThoughGameProgramming-4thEdition/score_rater_2.cpp
@@ -12,7 +12,7 @@ int main() {
     int score;
     char again = 'y';
 
-    while (again == 'y') {
+    do {
         cout << "Enter the score !" << endl;
         cin >> score;
         while (score) {
@@ -21,7 +21,7 @@ int main() {
         }
         cout << "Do you want to play again ? " ;
         cin >> again;
-    }
+    }while (again == 'y');
     cout << "OK bye ! " << endl;
     return 0;
 }
