19164c8aa2db3f58ad89b9483cf8b8db0343b418
smiroshnikov <sergeim.qa@gmail.com>
2016-12-07 23:24:57 +0200

    what does random seed means

--- BeginningC++ThoughGameProgramming-4thEdition/dice-rollerV1.cpp
+++ BeginningC++ThoughGameProgramming-4thEdition/dice-rollerV1.cpp
@@ -14,7 +14,27 @@ const int requiredDice = 6; // for convenient control
 
 int main() {
 
+    /*
+     * Computers generate pseudorandom numbers—not truly random numbers—based on a formula.
+     * One way to think about this is to imagine that the computer reads from a huge book of
+     * predetermined numbers. By reading from this book, the computer can appear to produce a
+     * sequence of random numbers.But there’s a problem: The computer always starts reading the
+     * book from the beginning. Because of this, the computer will always produce the same
+     * series of “random” numbers in a program. In games, this isn’t something we’d want.
+     * We wouldn’t, for example, want the same series of dice rolls in a game of craps every
+     * time we played.A solution to this problem is to tell the computer to start reading from
+     * some arbitrary place in the book when a game program begins. This process is called seeding
+     * the random number generator. Game programmers give the random number generator a number,
+     * called a seed, to determine the starting place in this sequence of pseudorandom numbers
+     **/
+
     srand(static_cast<unsigned int>(time(0))); // seed random number generator - WTF?
+    /*
+     * Wow, that’s a pretty cryptic looking line, but what it does is simple.
+     * It seeds the random number generator based on the current date and time,
+     * which is perfect since the current date and time will be different for each run of the program.
+     * */
+
     int randomNumber; // generate random number
     int characterRolls[allowedRolls];
 
@@ -27,7 +47,7 @@ int main() {
         while (i < allowedRolls) {
             randomNumber = rand();
             //cout << RAND_MAX << endl; // OK , const in rand , just to see my limit
-         //   cout << "You rolled a " << (randomNumber % 6) + 1 << endl; // significantly slows down execution time
+            //   cout << "You rolled a " << (randomNumber % 6) + 1 << endl; // significantly slows down execution time
             characterRolls[i] = randomNumber % requiredDice + 1;
             i++;
         }
